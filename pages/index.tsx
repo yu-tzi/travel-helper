2f8d02da623852feea077df24ccf82ed7708066c
kiki <zoeluoluo05673@gmail.com>
2024-01-27 19:01:56 +0800

    feat: 增加行程總覽的 UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,27 @@
 import type { Liff } from "@line/liff";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { format } from "date-fns";
-import { HiOutlinePlusSm } from "react-icons/hi";
+import { format, isBefore, isSameDay } from "date-fns";
+import { HiOutlineClock, HiOutlinePlusSm } from "react-icons/hi";
+import tours from "../data/tours.json";
+import { useMemo } from "react";
 
 const Home: NextPage<{
   liff: Liff | null;
   liffError: string | null;
 }> = ({ liff, liffError }) => {
+  const formattedTours = useMemo(() => {
+    let date = tours[0].timestamp;
+    return tours.map((tour, index) => {
+      const isFirstShowDate =
+        !isSameDay(new Date(tour.timestamp), new Date(date)) || index === 0;
+      date = tour.timestamp;
+      return {
+        ...tour,
+        isFirstShowDate,
+      };
+    });
+  }, []);
   return (
     <div>
       <Head>
@@ -44,7 +58,53 @@ const Home: NextPage<{
                   {format(new Date(), "E dd LLL yyyy")}
                 </div>
               </div>
-              <div className="text-xs font-medium text-slate-400">JOURNEY</div>
+              <div>
+                <div className="text-xs font-medium text-slate-400 mb-2">
+                  JOURNEY
+                </div>
+                <div className="overflow-scroll h-[60vh] py-1 snap-y">
+                  <div className="relative">
+                    <div className="h-full w-1 bg-[#866ce7e9] absolute top-0 left-6"></div>
+                    {formattedTours.map((tour) => {
+                      return (
+                        <div
+                          key={tour.id}
+                          className={`flex flex-row items-center snap-start ${
+                            tour.isFirstShowDate
+                              ? "justify-between"
+                              : "justify-end"
+                          }`}
+                        >
+                          {tour.isFirstShowDate && (
+                            <div className="relative rounded-full border-[3px] bg-white border-[#866ce7e9] h-12 w-12 flex justify-center items-center">
+                              <div className="text-sm text-[#866ce7e9]">
+                                {format(new Date(tour.timestamp), "MM/dd")}
+                              </div>
+                            </div>
+                          )}
+                          <div className="bg-white text-[#866ce7e9] border-2 border-neutral-200 w-48 h-18 rounded-md mb-3 flex flex-col justify-start pt-1.5 pb-2 px-2">
+                            <div className="text-sm font-medium truncate">
+                              {tour.name}
+                            </div>
+                            <div className="text-xs opacity-80">
+                              {isBefore(new Date(tour.timestamp), new Date())
+                                ? "upcoming"
+                                : "passed"}
+                            </div>
+                            <div className="flex flex-row justify-end items-center gap-1">
+                              <HiOutlineClock />
+                              <div className="text-xs">
+                                {format(new Date(tour.timestamp), "HH:mm")}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+
               {/*
                 <button
                   className="bg-gray-400 hover:bg-gray-200 text-white font-sm py-2 px-4 rounded w-40 m-auto"
