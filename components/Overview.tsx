07f7fe5865e0570406db20a90057d45d38dc7479
kiki <zoeluoluo05673@gmail.com>
2024-03-09 12:37:19 +0800

    feat: 增加篩選條件彈窗

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -10,20 +10,28 @@ import {
   StatGroup,
   useDisclosure,
   Button,
-} from "@chakra-ui/react";
-import { Box } from "@chakra-ui/react";
-import { Divider } from "@chakra-ui/react";
-import { Tag, TagLabel, TagLeftIcon } from "@chakra-ui/react";
-import { HiFilter, HiSortDescending } from "react-icons/hi";
-import {
+  Box,
+  Divider,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
   Modal,
   ModalContent,
   ModalHeader,
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  AbsoluteCenter,
+  Portal,
+  Select,
+  VStack,
+  Input,
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
 } from "@chakra-ui/react";
-import { Portal } from "@chakra-ui/react";
+import { HiFilter, HiSortDescending } from "react-icons/hi";
 
 export const Overview = ({ liff }: { liff: Liff | null }) => {
   const [accessToken, setAccessToken] = useState("");
@@ -31,6 +39,11 @@ export const Overview = ({ liff }: { liff: Liff | null }) => {
     isOpen: isFilterOpen,
     onOpen: onFilterOpen,
     onClose: onFilterClose,
+  } = useDisclosure();
+  const {
+    isOpen: isSortOpen,
+    onOpen: onSortOpen,
+    onClose: onSortClose,
   } = useDisclosure();
   useEffect(() => {
     const accessToken = liff?.isLoggedIn() && liff?.getIDToken();
@@ -75,23 +88,87 @@ export const Overview = ({ liff }: { liff: Liff | null }) => {
           <TagLeftIcon boxSize="12px" as={HiFilter} />
           <TagLabel>設定篩選條件</TagLabel>
         </Tag>
-        <Tag size="lg" colorScheme="cyan" cursor="pointer">
+        <Tag size="lg" colorScheme="cyan" cursor="pointer" onClick={onSortOpen}>
           <TagLeftIcon boxSize="12px" as={HiSortDescending} />
           <TagLabel>設定排序條件</TagLabel>
         </Tag>
       </div>
-      {isFilterOpen && (
+      {(isFilterOpen || isSortOpen) && (
         <Portal>
           <div className="w-[100vw] h-[100vh] bg-black absolute top-0 left-0 opacity-80"></div>
         </Portal>
       )}
       <Modal isOpen={isFilterOpen} onClose={onFilterClose}>
         <ModalContent maxWidth="375px" width="50vh">
+          <ModalHeader>設定篩選條件</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack
+              divider={
+                <Box position="relative">
+                  <Divider borderColor="gray.200" />
+                  <AbsoluteCenter bg="white" px="4">
+                    🔽
+                  </AbsoluteCenter>
+                </Box>
+              }
+              spacing={4}
+              align="stretch"
+            >
+              <Select placeholder="尚未選擇篩選項目">
+                <option value="option1">日期</option>
+                <option value="option2">停留時間</option>
+                <option value="option3">優先度</option>
+              </Select>
+              <Select placeholder="尚未選擇 Operator">
+                <option value="option1">大於</option>
+                <option value="option2">大於等於</option>
+                <option value="option3">等於</option>
+                <option value="option3">小於等於</option>
+                <option value="option3">小於</option>
+              </Select>
+              <Input placeholder="Select Date" size="md" type="date" />
+              <VStack spacing={1}>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                ></Box>
+                <RangeSlider
+                  aria-label={["min", "max"]}
+                  onChangeEnd={(val) => console.log(val)}
+                  min={0}
+                  max={30}
+                  step={1}
+                >
+                  <RangeSliderTrack>
+                    <RangeSliderFilledTrack />
+                  </RangeSliderTrack>
+                  <RangeSliderThumb index={0} />
+                  <RangeSliderThumb index={1} />
+                </RangeSlider>
+              </VStack>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              width={"150px"}
+              size={"md"}
+              onClick={onFilterClose}
+            >
+              套用
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Modal isOpen={isSortOpen} onClose={onSortClose}>
+        <ModalContent maxWidth="375px" width="50vh">
           <ModalHeader>Modal Title</ModalHeader>
           <ModalCloseButton />
           <ModalBody></ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onFilterClose}>
+            <Button colorScheme="blue" mr={3} onClick={onSortClose}>
               Close
             </Button>
             <Button variant="ghost">Secondary Action</Button>
